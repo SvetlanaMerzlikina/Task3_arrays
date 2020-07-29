6c7983d1ba3be1a305335bdb8c2573a003d80e74
Мерзликина Светлана Юрьевна <s_merzlikina92@mail.ru>
2020-07-29 10:24:18 +0300

    Array corrections

--- src/arrays.java
+++ src/arrays.java
@@ -1,11 +1,13 @@
 public class arrays {
     public static void main(String[] args) {
+        System.out.println("Массив: ");
         int[] array = {-856, 57, 400, -9, 0, 38, 277, 998, 999, 597, 88, 647, -90, 300,
                 -555, -60, 2, 17, -604, 408, 23, 222, -222, 90, 852, 5, 64, 741, -32, 96};
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
         System.out.println();
+        System.out.println("Вычисление факториала: ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 0 && array[i] < 10) {
                 int result = 1;
@@ -16,8 +18,9 @@ public class arrays {
             }
         }
         System.out.println();
+        System.out.println("Манипуляция с двузначными числами: ");
         for (int i = 0; i < array.length; i++) {
-            if ((array[i] > 10 && array[i] < 100) || (array[i] < -10 && array[i] > -100)) {
+            if ((array[i] > 9 && array[i] < 100) || (array[i] < -9 && array[i] > -100)) {
                 if (array[i] % 2 == 0) {
                     System.out.println("Четные: " + array[i] + " " + array[i] / 2);
                 } else
@@ -25,24 +28,25 @@ public class arrays {
             }
         }
         System.out.println();
+        System.out.println("Максимальное трехзначное число: ");
         int max = array[1];
         for (int i = 1; i < array.length; i += 2) {
-            if (array[i] >= 100 && array[i] < 1000) {
+            if ((array[i] >= 100 && array[i] < 1000) || (array[i] <= -100 && array[i] > -1000)) {
                 if (array[i] > max)
                     max = array[i];
             }
         }
-        System.out.println("Максимальное трехзначное: " + max);
+        System.out.println(max);
         System.out.println();
-
+        System.out.println("Минимальное трехзначное число: ");
         int min = array[0];
         for (int i = 0; i < array.length; i += 2) {
-            if (array[i] <= -100 && array[i] > -1000) {
+            if ((array[i] <= -100 && array[i] > -1000) || (array[i] <= -100 && array[i] > -1000)) {
                 if (array[i] < min)
                     min = array[i];
             }
         }
-        System.out.println("Минимальное трехзначное: " + min);
+        System.out.println(min);
     }
 }
 
